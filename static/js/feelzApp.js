5d7da07bd6059eba1ad2510d54aa2f11cd45179c
Monica <mcl018@ucsd.edu>
2015-01-27 17:59:40 -0800

    add panels to hold posts

--- static/js/feelzApp.js
+++ static/js/feelzApp.js
@@ -2,8 +2,10 @@
 
 // add the following:
 
-// update main post page with selected post overlay
+// update main post page with selected post pop up
 
 // bump a post script
 
 // update when new post is added
+
+//make newpost page into a popup form
