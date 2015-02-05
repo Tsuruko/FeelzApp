e250439a2f584eb0e596aac3884f470ff70694ab
Kevin Hsu <keh024@ucsd.edu>
2015-02-04 23:09:22 -0800

    added forgot password page

---
+++ static/js/login.js
@@ -0,0 +1,22 @@
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+    initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+  $('#signupButton').click(signupbox);
+  $('#signupButton').click(test);
+}
+
+function test(e) {
+  console.log(working);
+}
+
+//show signup form and hide login
+function signup(e) {
+  $('#loginbox').hide(); 
+  $('#signupbox').show();
+}
