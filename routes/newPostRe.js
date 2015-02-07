6d415ff1f46506a94854caa676cbab4c5a0f14b8
Monica <mcl018@ucsd.edu>
2015-02-06 19:35:09 -0800

    newPost categories added

--- routes/newPostRe.js
+++ routes/newPostRe.js
@@ -3,6 +3,7 @@ var data = require('../postData.json');
 
 exports.pushPost = function(req, res){
 
+  var cat = req.query.newPostCategory;
   var postTitle = req.query.newPostTitle;
   var postContent = req.query.newPostContent;
   var snippet = postContent;
@@ -11,7 +12,23 @@ exports.pushPost = function(req, res){
     var ellipsis = "...";
     snippet = postContent.substring(0,71).concat(ellipsis);
   }
-  var newPost = { "postCategory": "panel-default",
+
+  var newCategory = "";
+  if (cat == 1) {
+    newCategory = "panel-default";
+  } else if (cat == 2) {
+    newCategory = "panel-primary";
+  } else if (cat == 3) {
+    newCategory = "panel-success";
+  } else if (cat == 4) {
+    newCategory = "panel-info";
+  } else if (cat == 5) {
+    newCategory = "panel-warning";
+  } else if (cat == 6) {
+    newCategory = "panel-danger";
+  }
+
+  var newPost = { "postCategory": newCategory,
                   "postTitle": postTitle,
                   "postInfo": snippet,
                   "fullPost": postContent
