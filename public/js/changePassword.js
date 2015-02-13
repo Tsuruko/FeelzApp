095329dae7de44495605836ec9cf7be09cc00d65
Monica <mcl018@ucsd.edu>
2015-02-12 20:09:32 -0800

    added some password checks

--- public/js/changePassword.js
+++ public/js/changePassword.js
@@ -1,6 +1,3 @@
-confirmNewPassword
-
-
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
     initializePage();
@@ -13,6 +10,7 @@ function initializePage() {
 	console.log("test");
 	hidePasswordForm();
 	$("#changePasswordButton").click(changePass);
+  $(".alert").hide();
 }
 
 function hidePasswordForm() {
@@ -30,7 +28,61 @@ function changePass(e) {
       return text === "Change password" ? "Back" : "Change password";
   });
 
-
 $("#changePassword").hide();
 
 }
+
+  function checkForm(form)
+  { /*
+    if(form.username.value == "") {
+      alert("Error: Username cannot be blank!");
+      form.username.focus();
+      return false;
+    }
+    re = /^\w+$/;
+    if(!re.test(form.username.value)) {
+      alert("Error: Username must contain only letters, numbers and underscores!");
+      form.username.focus();
+      return false;
+    }  */
+
+    var re = /[]/;
+    if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
+      if(form.pwd1.value.length < 6) {
+        alert("Error: Password must contain at least six characters!");
+        form.pwd1.focus();
+        return false;
+      }
+      if(form.pwd1.value == form.oldPassword.value) {
+        alert("Error: New password must be different from old password!!");
+        form.pwd1.focus();
+        return false;
+      }
+      re = /[0-9]/;
+      if(!re.test(form.pwd1.value)) {
+        alert("Error: password must contain at least one number (0-9)!");
+        form.pwd1.focus();
+        return false;
+      }
+      re = /[a-z]/;
+      if(!re.test(form.pwd1.value)) {
+        alert("Error: password must contain at least one lowercase letter (a-z)!");
+        form.pwd1.focus();
+        return false;
+      }
+      re = /[A-Z]/;
+      if(!re.test(form.pwd1.value)) {
+        alert("Error: password must contain at least one uppercase letter (A-Z)!");
+        form.pwd1.focus();
+        return false;
+      }
+    } else {
+      alert("Error: Please check that you've entered and confirmed your password!");
+      form.pwd1.focus();
+      return false;
+    }
+
+    alert("You entered a valid password: " + form.pwd1.value);
+    $("#passwordAlert").hide();
+    return true;
+  }
--- routes/changePassword.js
+++ routes/changePassword.js
@@ -3,18 +3,19 @@ var data = require('../userData.json');
 
 exports.changePass = function(req, res) {
 	var old = req.query.oldPassword;
-	var newP = req.query.newPassword;
-	var confirm = req.query.confirmNewPassword;
+	var newP = req.query.pwd1;
+	var confirm = req.query.pwd2;
 
 	//find logged in as here
 	var loggedIn = data["users"][0];
 
 	if (old != loggedIn["password"]) {
 		console.log("error, password doesn't match old password");
+		return;
 	}
 
 	if (confirm != newP) {
-		console.log("error, passwords don't password");
+		console.log("error, passwords don't match");
 	}
 
 	loggedIn["password"] = newP;
