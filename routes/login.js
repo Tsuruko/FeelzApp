946c1905af827dd29f8e2c5adf96899759617e60
Kevin Hsu <keh024@ucsd.edu>
2015-02-13 03:22:15 -0800

    added some login js

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,28 @@
-// Get all of our post data
-//var data = require('../postData.json');
+// Get all of our user data
+var data = require('../userData.json');
 
 exports.viewLogin = function(req, res){
-	res.render('login');
+	var entered_username = req.query.login-username;
+	var entered_password = req.query.login-password;
+	
+	var count = 0;
+
+	while( count < data["users"].length ) {
+		if( entered_username == data["users"][count]["username"])
+			break;
+
+		count++;
+	}
+
+	if( count == data["users"].length) {
+		console.log("error, wrong username/password")
+		return;
+	}
+
+	if( entered_password != data["users"][count]["password"] ) {
+		console.log("error, wrong username/password");
+		return;
+	}
+	
+	res.render('login', LoggedIn);
 };
--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,33 @@
-// Get all of our post data
-//var data = require('../postData.json');
+// Get all of our previous user data
+var data = require('../userData.json');
 
 exports.viewSignup = function(req, res){
-	res.render('signup');
+
+	//var entered_email = req.query.signup_email;
+
+	/*
+	var entered_username = login-username;
+	var entered_password = login-password;
+	
+	var count = 0;
+
+	while( count < data["users"].length - 1 ) {
+		if( entered_username == data["users"][count]["username"])
+			break;
+
+		count++;
+	}
+
+	if( count == data["users"].length) {
+		console.log("error, wrong username/password")
+		return;
+	}
+
+	if( entered_password != data["users"][count]["password"] ) {
+		console.log("error, wrong username/password");
+		return;
+	}
+	*/
+	
+	res.render('signup', LoggedIn);
 };
